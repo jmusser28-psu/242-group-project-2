b7068d5eef485e247b420d00f46afe613079d0d7
jmusser28-psu <jmusser28@psu.edu>
2025-03-05 17:59:31 -0500

    Added some input validation, tried to make main nicer with user interaction

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,11 +20,25 @@ public class Main {
         boolean run = true;
         while (run) {
             if (isFirstTime) {
+                System.out.println("---Initial Setup---");
                 System.out.print("Would you like to enter yearly or monthly data? ");
                 String choice = GetString.get();
-                if (choice.equalsIgnoreCase("Monthly")) {
-                    isMonthly = true;
+                boolean isValid = false;
+                while (!isValid) {
+                    if (choice.equalsIgnoreCase("Monthly")) {
+                        isMonthly = true;
+                        isValid = true;
+                    }
+                    else if (choice.equalsIgnoreCase("Yearly")) {
+                        isMonthly = false;
+                        isValid = true;
+                    }
+                    else {
+                        System.out.print("Invalid choice, please enter a valid option: ");
+                        choice = GetString.get();
+                    }
                 }
+
                 housingExpenses = new HousingExpenses(isMonthly);
                 utilitiesExpenses = new UtilitiesExpenses(isMonthly);
                 foodExpenses = new FoodExpenses(isMonthly);
@@ -33,34 +47,46 @@ public class Main {
                 insuranceExpenses = new InsuranceExpenses(isMonthly);
                 isFirstTime = false;
             }
+
             else {
                 System.out.println("---Banking Program---");
-                System.out.println("1.) List Expenses");
-                System.out.println("2.) Set Expenses");
-                System.out.println("3.) ");
+                System.out.println("\t0.) Quit");
+                System.out.println("\t1.) List Expenses");
+                System.out.println("\t2.) Set Expenses");
+                System.out.println("\t3.) Set Income");
+                System.out.println("\t4.) Set Filing Status");
+                System.out.println("\t5.) Calculate Tax Amount Owed");
+                System.out.println("\t6.) Reset Program");
                 System.out.print("What would you like to do? ");
                 userChoice = ByteValidation.validate();
-                if (userChoice == 1) {
+                if (userChoice == 0) {
+                    run = false;
+                }
+
+                else if (userChoice == 1) {
                     double totalExpenses = (housingExpenses.getExpenses() + utilitiesExpenses.getExpenses() +
-                            foodExpenses.getExpenses() + travelExpenses.getExpenses() + entertainmentExpenses.getExpenses());
+                            foodExpenses.getExpenses() + travelExpenses.getExpenses() +
+                            entertainmentExpenses.getExpenses() + insuranceExpenses.getExpenses());
                     System.out.printf("Housing Expenses: $%.2f\n", housingExpenses.getExpenses());
                     System.out.printf("Utilities Expenses: $%.2f\n", utilitiesExpenses.getExpenses());
                     System.out.printf("Food Expenses: $%.2f\n", foodExpenses.getExpenses());
                     System.out.printf("Travel Expenses: $%.2f\n", travelExpenses.getExpenses());
                     System.out.printf("Entertainment Expenses: $%.2f\n", entertainmentExpenses.getExpenses());
+                    System.out.printf("Insurance Premiums: $%.2f\n", insuranceExpenses.getExpenses());
                     System.out.printf("Total Expenses: $%.2f\n", totalExpenses);
-                    System.out.println();
                 }
+
                 else if (userChoice == 2) {
-                    System.out.println("What expense would you like to modify?");
-                    System.out.println("1.) Housing Expenses");
-                    System.out.println("2.) Utilities Expenses");
-                    System.out.println("3.) Food Expenses");
-                    System.out.println("4.) Travel Expenses");
-                    System.out.println("5.) Entertainment Expenses");
-                    System.out.println("6.) Insurance Premiums");
+                    System.out.println("Expenses");
+                    System.out.println("\t1.) Housing Expenses");
+                    System.out.println("\t2.) Utilities Expenses");
+                    System.out.println("\t3.) Food Expenses");
+                    System.out.println("\t4.) Travel Expenses");
+                    System.out.println("\t5.) Entertainment Expenses");
+                    System.out.println("\t6.) Insurance Premiums");
                     System.out.print("What would you like to do? ");
                     userChoice = ByteValidation.validate();
+
                     if (userChoice == 1) {
                         housingExpenses.setExpenses();
                     }
@@ -83,14 +109,36 @@ public class Main {
                         System.out.printf("Invalid choice %d\n", userChoice);
                     }
                 }
+
                 else if (userChoice == 3) {
 
+                }
 
+                else if (userChoice == 4) {
+
+                }
+
+                else if (userChoice == 5) {
+
+                }
+
+                else if (userChoice == 6) {
+                    housingExpenses = null;
+                    utilitiesExpenses = null;
+                    foodExpenses = null;
+                    travelExpenses = null;
+                    entertainmentExpenses = null;
+                    insuranceExpenses = null;
+                    isFirstTime = true;
+                }
+
+                else {
+                    System.out.printf("Invalid option, %d\n", userChoice);
+                    System.out.println();
                 }
 
             }
         }
-
 
     }
 }
