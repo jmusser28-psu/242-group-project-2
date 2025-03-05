c6001414763637055b5378a092075f3f720f57fd
jmusser28-psu <jmusser28@psu.edu>
2025-03-05 15:30:26 -0500

    Added some input validation, tried to make main nicer with user interaction

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,12 +1,11 @@
 package main;
 
-import java.util.Scanner;
 import expenses.TravelExpenses;
+import util.ByteValidation;
+import util.StringValidation;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scnr = new Scanner(System.in);
-
         boolean isFirstTime = true;
         boolean isMonthly = false;
 
@@ -16,11 +15,12 @@ public class Main {
         while (!(userChoice == -1)) {
             if (isFirstTime) {
                 System.out.print("Would you like to enter yearly or monthly data: ");
-                String choice = scnr.nextLine();
+                String choice = StringValidation.validate();
                 if (choice.equalsIgnoreCase("Monthly")) {
                     isMonthly = true;
                 }
                 System.out.print("Please enter travel expenses: ");
+                System.out.println();
                 travelExpenses = new TravelExpenses(isMonthly);
                 isFirstTime = false;
             }
@@ -29,7 +29,7 @@ public class Main {
                 System.out.println("1.) list expenses");
                 System.out.println("2.) set expenses");
                 System.out.print("What would you like to do? ");
-                userChoice = scnr.nextInt();
+                userChoice = ByteValidation.validate();
                 if (userChoice == 1) {
                     System.out.printf("Housing Expenses:\t%.2f\n", travelExpenses.getExpenses());
                     System.out.printf("Utilities Expenses:\t%.2f\n", travelExpenses.getExpenses());
