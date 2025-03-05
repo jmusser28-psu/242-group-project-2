e0fb865cfd36069e21fd723c0591992558cc6c4c
jmusser28-psu <jmusser28@psu.edu>
2025-03-05 18:30:12 -0500

    Added some input validation, tried to make main nicer with user interaction

--- src/util/GetCharacter.java
+++ src/util/GetCharacter.java
@@ -6,6 +6,7 @@ public class GetCharacter {
     private static Scanner scnr = new Scanner(System.in);
 
     public static char get() {
+        // Here as a shortcut to getting the first character of a
         char returnValue = GetString.get().charAt(0);
 
         return returnValue;
--- src/util/GetString.java
+++ src/util/GetString.java
@@ -6,6 +6,7 @@ public class GetString {
     private static Scanner scnr = new Scanner(System.in);
 
     public static String get() {
+        // Here so that we do not need to create or call a scanner in individual classes
         String returnValue = scnr.nextLine();
 
         return returnValue;
