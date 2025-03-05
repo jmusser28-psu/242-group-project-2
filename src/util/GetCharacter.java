a71bd03c8be9a493d0f751a4191e4f3e9e130b87
jmusser28-psu <jmusser28@psu.edu>
2025-03-05 18:34:28 -0500

    Added some input validation, tried to make main nicer with user interaction

--- src/util/GetCharacter.java
+++ src/util/GetCharacter.java
@@ -6,7 +6,7 @@ public class GetCharacter {
     private static Scanner scnr = new Scanner(System.in);
 
     public static char get() {
-        // Here as a shortcut to getting the first character of a
+        // Here as a shortcut to getting the first character of a string
         char returnValue = GetString.get().charAt(0);
 
         return returnValue;
